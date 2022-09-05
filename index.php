f8a139a5348916a2f4085d4874f1d3f8cdcbf645
vitoriaalmd <vitoriaalmeid54@gmail.com>
2022-09-05 19:54:31 -0300

    05.09

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	
+	<div class="box-login">
+  
+    <form action="">
+      
+        <div class="itens">
+          <label>Nome ou e-mail:</label>
+          <input type="text" placeholder="Digite aqui seu nome ou e-mail">
+        </div>
+      
+        <div class="itens">
+          <label>Senha:</label>
+          <input type="password" placeholder="*********">
+        </div>
+      
+        <div class="itens">
+          <a href="form.php"> <button class="submit"> Entrar </button> </a>
+        </div>
+      
+  </form>
+</div>
+
+
+</body>
+</html>
