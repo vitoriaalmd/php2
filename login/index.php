7fb39ae2faa6dd60de5fd41c5ca0f515b67df9fe
vitoriaalmd <vitoriaalmeid54@gmail.com>
2022-09-06 11:13:29 -0300

    alt 6.9.22

--- login/index.php
+++ login/index.php
@@ -6,7 +6,8 @@
 	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+<!-- CSS only -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> -->
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
@@ -19,19 +20,21 @@
 
         <div class="itens">
           <label>E-mail:</label>
-          <input type="text" placeholder="Digite aqui seu e-mail">
+          <input type="text" placeholder="Digite aqui seu e-mail" >
         </div>
       
         <div class="itens">
           <label>Senha:</label>
-          <input type="password" placeholder="*********">
+          <input type="password" placeholder="*********" >
         </div>
       
         <div class="itens">
-          <a href="form.php"> 
-            <input type="button" class="submit"> Entrar 
+          
+          <a class="submit" href="form.php"> 
+            <p for="" id="entrar">Entrar</p>
           </a>
         </div>
+
       
   </form>
 </div>
